96f039cee571475d9166e15d3f172ab6fc54aec7
sinner <yura224k@gmail.com>
2019-09-22 20:08:13 +0300

    API

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel TEST
+                    API
                 </div>
 
                 <div class="links">
