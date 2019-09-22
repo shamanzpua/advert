1db1c589d8150b4ce72b0a893f3b807b1ce1dc72
sinner <yura224k@gmail.com>
2019-09-22 12:43:53 +0300

    fix

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel TEST
+                    Laravel TEST 123
                 </div>
 
                 <div class="links">
