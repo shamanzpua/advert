a0b50c52ae17bd076cab2764377a5e2db6dca894
sinner <yura224k@gmail.com>
2019-09-22 20:12:32 +0300

    API

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    API
+                    API 123
                 </div>
 
                 <div class="links">
